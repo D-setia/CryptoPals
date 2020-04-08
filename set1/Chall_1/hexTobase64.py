89c230b2ea0a4e1747b3f19fe555e6884a3fbb27
dsetia <dsetia@cs.iitr.ac.in>
2020-04-08 18:00:35 +0530

    Add S1 C1 script

---
+++ set1/Chall_1/hexTobase64.py
@@ -0,0 +1,27 @@
+import base64
+import pprint
+
+def asciiToHexString(ip):
+    ip = ip.encode('utf-8')
+    hexString = ip.hex()
+    return hexString
+
+def hexStringToBytes(ip):
+    byteArray = bytes.fromhex(ip)
+    return byteArray
+
+def bytesTob64(ip):
+    b64String = base64.b64encode(ip)
+    return b64String
+
+def hexStringToAscii(ip):
+    string = bytearray.fromhex(ip).decode()
+    return string
+
+def main():
+    #ip = input("Give your input: ")
+    #print(hexStringToAscii(asciiToHexString(ip)))
+
+
+if __name__ == "__main__":
+    main()
