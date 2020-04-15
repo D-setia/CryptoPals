38c2ca13bfe0ca1f271b78e8331771baee8a1bc3
dsetia <dsetia@cs.iitr.ac.in>
2020-04-15 23:36:30 +0530

    Add S2 C15 script

---
+++ set2/chall_15/README.md
@@ -0,0 +1,1 @@
+Code for this is added to the script of challenge 9
--- set2/chall_9/implementPKCS7.py
+++ set2/chall_9/implementPKCS7.py
@@ -11,12 +11,23 @@ def PKCS7(ipByteArr, blockSize):
     paddedResult = bytes(paddedResult)
     return paddedResult
 
+
+def PKCS7validate(paddedPtBytes):
+    isValid = True
+    pad = paddedPtBytes[len(paddedPtBytes)-1]
+    for i in range(pad):
+        if paddedPtBytes[len(paddedPtBytes)-1-i] != pad:
+            isValid = False
+    return isValid
+
 def main():
     ip = "YELLOW SUBMARINE"
-    blocksize = 20
+    blocksize = 16
 
     ip = bytes(ip.encode('utf-8'))
     paddedIp = PKCS7(ip, blocksize)
+    isValid = PKCS7validate(paddedIp)
+    print(isValid)
     print(paddedIp)
 
 
