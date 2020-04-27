5fa025d69062e35655461bc29e83ca27221b9f81
dsetia <dsetia@cs.iitr.ac.in>
2020-04-27 15:12:48 +0530

    Add S4 C29 script

---
+++ set4/chall_29/breakSHA1MACusingLenExtension.py
@@ -0,0 +1,85 @@
+import hashlib
+import random
+import sys
+sys.path.insert(1, "../../")
+import helperFunctions as helpers
+import set4.chall_28.mysha1 as mysha1
+import set4.chall_28.implementSHA1keyedMAC as SHA1keyedMAC
+
+GLOBAL_KEY = None
+KEY_SIZE = None
+
+def addGluePadding(ipBytes, keySizeInBytes):
+    length = (len(ipBytes) + keySizeInBytes)*8
+    padLenInBits = 448 - length%512
+    if padLenInBits <= 0:
+        padLenInBits += 512
+    padLen = padLenInBits//8
+    pad = '\x80'+'\x00'*(padLen-1)
+    pad = bytes(pad.encode('latin1'))
+
+    messageLenPad = (bin(length)[2:]).zfill(64)
+    messageLenPad = messageLenPad[len(messageLenPad)-64:]
+    for i in range(8):
+        pad += bytes([int(messageLenPad[i*8 : (i+1)*8], 2)])
+
+    paddedIp = ipBytes+pad
+    return paddedIp
+
+
+def generateMac(ipBytes):
+    global GLOBAL_KEY
+    global KEY_SIZE
+
+    if KEY_SIZE == None:
+        KEY_SIZE = random.randrange(7,20)
+        #print(KEY_SIZE)
+    if GLOBAL_KEY == None:
+        GLOBAL_KEY = helpers.getRandBytes(KEY_SIZE)
+
+    mac = SHA1keyedMAC.SHA1MAC(ipBytes, GLOBAL_KEY)
+    mac2 = mysha1.sha1(GLOBAL_KEY+ipBytes)
+    return mac
+
+
+def forgeHash(originalTextString, newTextString):
+    ip = originalTextString
+    ip = bytes(ip.encode('utf-8'))
+    legitMac = generateMac(ip)
+
+    h = [0 for i in range(5)]
+    for i in range(len(legitMac)//4):
+        h[i] = int((legitMac[i*4: (i+1)*4]).hex(), 16)
+
+    keySize = 0
+    for i in range(5, 33):
+        paddedIp = addGluePadding(ip, i)
+        payload = newTextString
+        payload = bytes(payload.encode('latin1'))
+        prefixLenInBytes = len(paddedIp) + i
+
+        legitMac2 = generateMac(paddedIp+payload)
+        forgedMac = mysha1.sha1(payload, prefixLenInBytes, h[0], h[1], h[2], h[3], h[4])
+        if legitMac2 == forgedMac:
+            keySize = i
+            validText = paddedIp + payload
+            return validText, forgedMac, keySize
+    
+    return None, None, None
+    
+
+
+def main():
+    originalText = "comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+    newText = ";admin=true"
+    validText, forgedHash, keySize = forgeHash(originalText, newText)
+    legitHash = generateMac(validText)
+    print(legitHash)
+    print(forgedHash)
+    #print(keySize)
+    
+
+
+if __name__ == "__main__":
+    main()
+
