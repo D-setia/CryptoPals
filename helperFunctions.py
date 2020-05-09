428eb0db329f3d41cf8ef7894db7c9f042dda98c
dsetia <dsetia@cs.iitr.ac.in>
2020-05-09 19:18:10 +0530

    Implement modular inversion

--- helperFunctions.py
+++ helperFunctions.py
@@ -74,14 +74,83 @@ def changeEndianness(a):
     res = ((a & 0xff) << 24) | ((a & 0xff00) << 8) | ((a & 0xff0000) >> 8) |((a & 0xff000000) >> 24)
     return res
 
+def modInv(x, N):
+    a = []
+    b = []
+    q = []
+    r = []
+
+    A = N
+    B = x
+    Q = A//B
+    R = A%B
+    a.append(A)
+    b.append(B)
+    q.append(Q)
+    r.append(R)
+
+    while R != 0:
+        A = B 
+        B = R
+        Q = A//B
+        R = A%B
+        a.append(A)
+        b.append(B)
+        q.append(Q)
+        r.append(R)
+    
+    if B != 1:
+        return None
+    else:
+        a.pop()
+        b.pop()
+        q.pop()
+        r.pop()
+        inv = 0
+        while len(q) > 1:
+            q2 = q.pop()
+            q1 = q.pop()
+            r2 = r.pop()
+            r1 = r.pop()
+            a2 = a.pop()
+            a1 = a.pop()
+            b2 = b.pop()
+            b1 = b.pop()
+
+            r.append(0-r2)
+            b.append(b1)
+            q.append(q1*q2 + a2//b1)
+            a.append(a1*q2)
+
+        if len(q) == 0:
+            inv = 1
+        else:
+            R = r.pop()
+            Q = q.pop()
+            if R > 0:
+                inv = 0-Q
+            else:
+                inv = Q
+            inv = inv%N
+
+        return inv
+
 
 def main():
     andString = binaryStringXor('01001100', '00101010')
-    print(andString)
+    #print(andString)
     randBytes = getRandBytes(16)
-    print(randBytes)
-    print(pow(2,32)-1)
-    print(leftRotate(pow(2,32)-1, 30))
+    #print(randBytes)
+    #print(pow(2,32)-1)
+    #print(leftRotate(pow(2,32)-1, 30)) 
+    for j in range(20, 100):
+        for i in range(1, j):
+            inv = modInv(i, j)
+            if inv != None:
+                print((i*inv)%j)
+
+        print('')
+    
 
 
 if __name__ == "__main__":
